2d48d76f92788a11c78ddf0cb40590b5651272d0
Martin <meatole.cs@gmail.com>
2024-05-06 18:40:32 +0800

    fix deployed: cannot dedploy because module not found header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Header } from "@/components/ui/header";
+import  { Header } from "@/components/ui/header";
 import PokemonContainer from "@/components/catalogue/PokemonContainer";
+
 
 export default function Page() {
   return (
