6241012ec2f918ec3f2eff4ebaeb20c6d9778be2
Martin <meatole.cs@gmail.com>
2024-05-06 18:50:37 +0800

    feat: Renamed app title to Pokedex and added app description

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PokeDex",
-  description: "",
+  title: "Pokedex",
+  description: "A pokemon encyclopedia",
 };
 
 export default function RootLayout({ children }) {
