a16f926699309d4c809685e5bcd0649060ecc6f9
Martin Edgar Atole <107755242+CS-Martin@users.noreply.github.com>
2024-06-21 01:42:47 +0800

    Delete pokedexts directory

--- pokedexts/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import CatalogueContainer from "./_components/catalogue-container";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CatalogueContainer />
-    </main>
-  );
-}
